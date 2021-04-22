0257b7279d4320bea2237143baf8ae0473a9a986
lilliantyiso@gmail.com <lilliantyiso@gmail.com>
2021-04-22 13:00:26 +0200

    finished radio functions

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,22 +1,53 @@
 var itemTypeRadio = document.querySelector(".billItemTypeRadio")
 // get a reference to the sms or call radio buttons
-var radioAddBtn = document.querySelector(".radionBillAddBtn")
+var radioAddBtn = document.querySelector(".radioBillAddBtn")
 //get a reference to the add button
+var totalCallsElement = document.querySelector(".callTotalTwo")
+var totalSmsElement = document.querySelector(".smsTotalTwo")
 var radioTotal = document.querySelector(".totalTwo")
+var randCol = document.querySelector(".orange")
 //create a variable that will keep track of the total bill
-radioAddBtn.addEventListener('click', radioButtonBill)
-//add an event listener for when the add button is pressed
-var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-if (checkedRadioBtn){
-    var billItemType = checkedRadioBtn.value
-}
-function radionBillAddBtn(){
+var totalCalls = 0;
+var totalSms = 0;
+var costTotal = 0
+
+function radioButtonBill(){
     var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-if (checkedRadioBtn){
-    var billItemType = checkedRadioBtn.value
+    if (checkedRadioBtn){
+        var billItemType = checkedRadioBtn.value.trim()
+        if (billItemType === "call"){
+            totalCalls += 2.75;
+            costTotal += 2.75;
+        }
+        else if (billItemType === "sms"){
+            totalSms += 0.75;
+            costTotal += 0.75;
+        }
+        if ( costTotal >= 20 && costTotal < 30){
+            // adding the danger class will make the text red
+            radioTotal.classList.add("warning");
+            randCol.classList.add("warning");
+        }
+        else if (costTotal >= 30){
+            radioTotal.classList.remove("warning");
+            randCol.classList.remove("warning");
+            radioTotal.classList.add("danger");
+            randCol.classList.add("danger");
+            
+        }
+    }
+    // update the correct total
+    
+    //update the totals that is displayed on the screen.
+    totalCallsElement.innerHTML = totalCalls.toFixed(2);
+    totalSmsElement.innerHTML = totalSms.toFixed(2);
+    radioTotal.innerHTML = costTotal.toFixed(2);
+     console.log(totalCalls)
+     console.log(totalSms)
+    console.log(radioTotal)
+
 }
-}
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+radioAddBtn.addEventListener('click', radioButtonBill);
+
+
+
