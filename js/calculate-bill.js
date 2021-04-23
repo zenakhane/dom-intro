88fb87fcbea30391ca87e10f012e37d9af140c56
lilliantyiso@gmail.com <lilliantyiso@gmail.com>
2021-04-23 16:43:01 +0200

    done

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -14,51 +14,42 @@ function calculateBtnClicked(){
     var billItems = billString.split(",");
     // a variable for the total phone bill.
     var billTotal = 0;
-            colorElement1.classList.remove("warning");
-            billTotalElement.classList.remove("warning");
-            colorElement1.classList.remove("danger");
-            billTotalElement.classList.remove("danger");
+    colorElement1.classList.remove("warning");
+    billTotalElement.classList.remove("warning");
+    colorElement1.classList.remove("danger");
+    billTotalElement.classList.remove("danger");
     //loop over all the bill items
     for (var i=0;i<billItems.length;i++){
-        var billItem = billItems[i].trim();
-        if (billItem === "call"){
-            billTotal += 2.75;
-        }
-        else if (billItem === "sms"){
-            billTotal += 0.75;
-        }
-
-        if (billTotal >= 20 && billTotal < 30){ 
-            colorElement1.classList.add("warning");
-            billTotalElement.classList.add("warning");
-            console.log(colorElement1)
-        }
-        else if (billTotal >= 30){
-            billTotalElement.classList.add("danger");
-            colorElement1.classList.add("danger");
-            billTotalElement.classList.remove("warning");
-            colorElement1.classList.remove("warning");
-
-            console.log(colorElement1)
-            
-        }
-        else{
-            colorElement1.classList.remove("total");
-            billTotalElement.classList.remove("warning");
-            colorElement1.classList.remove("total");
-            billTotalElement.classList.remove("danger");
-            console.log(colorElement1)
-
-        }
-
-        
-
+    var billItem = billItems[i].trim();
+    if (billItem === "call"){
+    billTotal += 2.75;
     }
-    
+    else if (billItem === "sms"){
+    billTotal += 0.75;
+    }
+    if (billTotal >= 20 && billTotal < 30){ 
+    colorElement1.classList.add("warning");
+    billTotalElement.classList.add("warning");
+    console.log(colorElement1)
+    }
+    else if (billTotal >= 30){
+    billTotalElement.classList.add("danger");
+    colorElement1.classList.add("danger");
+    billTotalElement.classList.remove("warning");
+    colorElement1.classList.remove("warning");
+    console.log(colorElement1)
+    }
+    else{
+    colorElement1.classList.remove("total");
+    billTotalElement.classList.remove("warning");
+    colorElement1.classList.remove("total");
+    billTotalElement.classList.remove("danger");
+    console.log(colorElement1)
+    }
+    }
     //round to two decimals
     var roundedBillTotal = billTotal.toFixed(2);
     billTotalElement.innerHTML = roundedBillTotal;
     console.log(roundedBillTotal)
-}
-
-calculateBtn.addEventListener('click',  calculateBtnClicked)
+    }
+    calculateBtn.addEventListener('click',  calculateBtnClicked)
--- js/radio-bill.js
+++ js/radio-bill.js
@@ -10,44 +10,37 @@ var randCol = document.querySelector(".orange")
 var totalCalls = 0;
 var totalSms = 0;
 var costTotal = 0
-
 function radioButtonBill(){
-    var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
-    if (checkedRadioBtn){
-        var billItemType = checkedRadioBtn.value.trim()
-        if (billItemType === "call"){
-            totalCalls += 2.75;
-            costTotal += 2.75;
-        }
-        else if (billItemType === "sms"){
-            totalSms += 0.75;
-            costTotal += 0.75;
-        }
-        if ( costTotal >= 20 && costTotal < 30){
-            // adding the danger class will make the text red
-            radioTotal.classList.add("warning");
-            randCol.classList.add("warning");
-        }
-        else if (costTotal >= 30){
-            radioTotal.classList.remove("warning");
-            randCol.classList.remove("warning");
-            radioTotal.classList.add("danger");
-            randCol.classList.add("danger");
-            
-        }
-    }
-    // update the correct total
-    
-    //update the totals that is displayed on the screen.
-    totalCallsElement.innerHTML = totalCalls.toFixed(2);
-    totalSmsElement.innerHTML = totalSms.toFixed(2);
-    radioTotal.innerHTML = costTotal.toFixed(2);
-     console.log(totalCalls)
-     console.log(totalSms)
-    console.log(radioTotal)
-
+var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+if (checkedRadioBtn){
+var billItemType = checkedRadioBtn.value.trim()
+if (billItemType === "call"){
+totalCalls += 2.75;
+costTotal += 2.75;
+}
+else if (billItemType === "sms"){
+totalSms += 0.75;
+costTotal += 0.75;
+}
+if ( costTotal >= 20 && costTotal < 30){
+// adding the danger class will make the text red
+radioTotal.classList.add("warning");
+randCol.classList.add("warning");
+}
+else if (costTotal >= 30){
+radioTotal.classList.remove("warning");
+randCol.classList.remove("warning");
+radioTotal.classList.add("danger");
+randCol.classList.add("danger");
+}
+}
+// update the correct total
+//update the totals that is displayed on the screen.
+totalCallsElement.innerHTML = totalCalls.toFixed(2);
+totalSmsElement.innerHTML = totalSms.toFixed(2);
+radioTotal.innerHTML = costTotal.toFixed(2);
+console.log(totalCalls)
+console.log(totalSms)
+console.log(radioTotal)
 }
 radioAddBtn.addEventListener('click', radioButtonBill);
-
-
-
